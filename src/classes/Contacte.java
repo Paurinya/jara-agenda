b03a0c3e64b7bf6a1f62666ff31c14ff51e3c2c9
paurinya <paulajara@ginebro.cat>
2024-09-20 17:36:54 +0200

    primer commit

---
+++ src/classes/Contacte.java
@@ -0,0 +1,10 @@
+package classes;
+
+import java.util.LinkedList;
+
+public class Contacte {
+    private String nom;
+    private String cognom;
+    private LinkedList<Telefon> llistaTelefons;
+    private LinkedList<Adressa> llistaAdreces;
+}
